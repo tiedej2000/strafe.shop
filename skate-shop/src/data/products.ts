022dd8d54e58df4a168c5e8e8256aae31c31c9cf
tiedej2000 <tiedejustin@protonmail.de>
2026-07-09 15:39:12 +0200

    seiten collection/produkte angelegt

---
+++ skate-shop/src/data/products.ts
@@ -0,0 +1,42 @@
+export const products = [
+	{
+		id: 'IDNR1',
+		title: 'SWEATER WR',
+		price: 120,
+		images: [
+			{ src: '/products/IDNR1/close_up3.webp', alt: 'Sweater WR – Vorderansicht' },
+			{ src: '/products/IDNR1/2.webp', alt: 'Sweater WR – Rückansicht' },
+			{ src: '/products/IDNR1/3.webp', alt: 'Sweater WR – Detail' },
+		],
+	},
+    {
+		id: 'IDNR2',
+		title: 'SWEATER WR',
+		price: 120,
+		images: [
+			{ src: '/products/IDNR2/close_up3.webp', alt: 'Sweater WR – Vorderansicht' },
+			{ src: '/products/IDNR2/2.webp', alt: 'Sweater WR – Rückansicht' },
+			{ src: '/products/IDNR2/3.webp', alt: 'Sweater WR – Detail' },
+		],
+	},
+    {
+		id: 'IDNR3',
+		title: 'SWEATER WR',
+		price: 120,
+		images: [
+			{ src: '/products/IDNR3/close_up3.webp', alt: 'Sweater WR – Vorderansicht' },
+			{ src: '/products/IDNR3/2.webp', alt: 'Sweater WR – Rückansicht' },
+			{ src: '/products/IDNR3/3.webp', alt: 'Sweater WR – Detail' },
+		],
+	},
+    {
+		id: 'IDNR4',
+		title: 'SWEATER WR',
+		price: 120,
+		images: [
+			{ src: '/products/IDNR4/close_up3.webp', alt: 'Sweater WR – Vorderansicht' },
+			{ src: '/products/IDNR4/2.webp', alt: 'Sweater WR – Rückansicht' },
+			{ src: '/products/IDNR4/3.webp', alt: 'Sweater WR – Detail' },
+		],
+	},
+]
